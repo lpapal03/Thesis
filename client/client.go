e7d3da62061ab638070db58e7c1718cc966411b2
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2022-10-15 12:15:03 +0300

    added poller

--- client/client.go
+++ client/client.go
@@ -5,52 +5,74 @@ package main
 import (
 	"fmt"
 	"math/rand"
-	"strconv"
 	"time"
 
 	zmq "github.com/pebbe/zmq4"
 )
 
 // Sender id is bound to the socket
-func get(s *zmq.Socket, msg_id int) {
-	msg := []string{strconv.Itoa(msg_id), "get"}
-	s.SendMessage(msg)
-	rec_msg, _ := s.RecvMessage(0)
-	fmt.Println("Server response:\n-------")
-	fmt.Println(rec_msg[1])
+// func get(s *zmq.Socket, msg_id int) {
+// 	msg := []string{strconv.Itoa(msg_id), "get"}
+// 	s.SendMessage(msg)
+// 	rec_msg, _ := s.RecvMessage(0)
+// 	fmt.Println("Server response:\n-------")
+// 	fmt.Println(rec_msg[1])
 
-	// Send request to 3f+1 servers
+// Send request to 3f+1 servers
 
-	// Wait response from 2f+1
-	// When waiting for responses,
+// Wait response from 2f+1
+// When waiting for responses,
 
-	// Foreach record r in each response, r should be in at least f+1 responses
-	// In other words, f+1 responses should match
-}
-
+// Foreach record r in each response, r should be in at least f+1 responses
+// In other words, f+1 responses should match
+// }
 func client_task(id string) {
+
 	rand.Seed(time.Now().UnixNano())
+	servers := []string{"tcp://localhost:5555", "tcp://localhost:5556"}
+	var server_sockets [2]*zmq.Socket
 	zctx, _ := zmq.NewContext()
+	poller := zmq.NewPoller()
 
 	// id := strconv.Itoa(os.Getpid()) + strconv.Itoa(rand.Intn(10))
 
-	s, _ := zctx.NewSocket(zmq.DEALER)
-	s.SetIdentity(id)
-	s.Connect("tcp://localhost:5555")
+	for i := 0; i < len(servers); i++ {
+		s, _ := zctx.NewSocket(zmq.DEALER)
+		s.SetIdentity(id)
+		s.Connect(servers[i])
+		server_sockets[i] = s
+		poller.Add(server_sockets[i], zmq.POLLIN)
+		fmt.Printf("Client with id %s connected to server %s\n", id, servers[i])
+		fmt.Println(server_sockets[i])
+		fmt.Println()
+	}
 
-	fmt.Printf("Client with id %s connected and bound\n", id)
+	server_sockets[1].SendMessage("Hello 1")
+	server_sockets[0].SendMessage("Hello 0")
 
-	msg_id := 1
-	get(s, msg_id)
-	msg_id++
+	for {
+		sockets, _ := poller.Poll(-1)
+		for _, socket := range sockets {
+			switch s := socket.Socket; s {
+			case server_sockets[0]:
+				msg, _ := s.Recv(0)
+				//  Process msg
+				fmt.Println(msg)
+			case server_sockets[1]:
+				msg, _ := s.Recv(0)
+				//  Process msg
+				fmt.Println(msg)
+			}
+		}
+	}
 
 }
 
 func main() {
 
 	go client_task("c1")
-	go client_task("c2")
-	go client_task("c3")
+	// go client_task("c2")
+	// go client_task("c3")
 
 	for {
 	}
--- server/server.go
+++ server/server.go
@@ -4,20 +4,11 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 
 	zmq "github.com/pebbe/zmq4"
 )
 
-func get(g_set map[string]string) string {
-	v := ""
-	for _, value := range g_set {
-		v += value + "\n"
-	}
-	return v
-}
-
-func server_task(id string, context *zmq.Context) {
+func server_task(port string, context *zmq.Context) {
 
 	g_set := make(map[string]string)
 	g_set["record1"] = "dog"
@@ -25,42 +16,34 @@ func server_task(id string, context *zmq.Context) {
 	g_set["record3"] = "cat"
 	g_set["record4"] = "red"
 
-	server_count := 5
+	// server_count := 5
 
 	inbound_socket, _ := context.NewSocket(zmq.ROUTER)
-	port := "tcp://*:5555"
 	inbound_socket.Bind(port)
 	fmt.Println("Client facing socket is up in port: ", port)
 
-	oubound_sockets := make([]*zmq.Socket, 0)
-	for i := 0; i < server_count; i++ {
-		s, _ := context.NewSocket(zmq.DEALER)
-		port := "tcp://*:1000" + strconv.Itoa(i)
-		s.Bind(port)
-		fmt.Println("Bound dealer to port:", port)
-		oubound_sockets = append(oubound_sockets, s)
-	}
-	fmt.Println("Server facing sockets are up!")
+	// oubound_sockets := make([]*zmq.Socket, 0)
+	// for i := 0; i < server_count; i++ {
+	// 	s, _ := context.NewSocket(zmq.DEALER)
+	// 	port := "tcp://*:1000" + strconv.Itoa(i)
+	// 	s.Bind(port)
+	// 	fmt.Println("Bound dealer to port:", port)
+	// 	oubound_sockets = append(oubound_sockets, s)
+	// }
+	// fmt.Println("Server facing sockets are up!")
 
 	for {
 		msg, _ := inbound_socket.RecvMessage(0)
-		fmt.Println(msg[0])
-		fmt.Println(msg[1])
-		fmt.Println(msg[2])
-
-		if msg[2] == "get" {
-			v := get(g_set)
-			response := []string{msg[0], id, v}
-			inbound_socket.SendMessage(response)
-		} else if msg[2] == "append" {
-			fmt.Println("Append not implemented yet")
-		}
+		fmt.Println(msg)
+		response := []string{msg[0], "World", port}
+		inbound_socket.SendMessage(response)
 	}
 }
 
 func main() {
 	zctx, _ := zmq.NewContext()
-	go server_task("s1", zctx)
+	go server_task("tcp://*:5555", zctx)
+	go server_task("tcp://*:5556", zctx)
 	for {
 	}
 }
