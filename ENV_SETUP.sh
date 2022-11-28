ae9d218435cdb5f2fd1b5fb4a6e96b88e719ff7a
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2022-11-28 19:15:43 +0200

    test

--- ENV_SETUP.sh
+++ ENV_SETUP.sh
@@ -1,3 +1,3 @@
-echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
-wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
-sudo apt-get install libzmq3-dev
+y | echo "deb http://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/ ./" >> /etc/apt/sources.list
+y | wget https://download.opensuse.org/repositories/network:/messaging:/zeromq:/release-stable/Debian_9.0/Release.key -O- | sudo apt-key add
+y | sudo apt-get install libzmq3-dev
