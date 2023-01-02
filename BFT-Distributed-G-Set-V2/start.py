6a71a3526e53bd409ebf48dfe3ae8aaf74d3ddb3
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-01-02 14:22:38 -0600

    test

--- BFT-Distributed-G-Set-V2/start.py
+++ BFT-Distributed-G-Set-V2/start.py
@@ -2,4 +2,38 @@ import os
 
 if __name__ == '__main__':
     os.system("go run server/main.go")
-    print('done')
+    print('done')import os
+import yaml
+
+def GetHosts():
+    hosts = []
+    for i in range(9999):
+        hostname = "node" + str(i)
+        response = os.system("ping -c 1 " + hostname)
+        if response == 0:
+            hosts.append(hostname)
+        else:
+            return hosts
+    
+
+
+if __name__ == '__main__':
+    # for normal execution
+    hosts = GetHosts()
+    f = open("hosts", "w")
+    for h in hosts:
+        f.write(h+"\n")
+    f.close
+
+    f = open("start_servers.yml", "w")
+    f.write("---\n")
+    f.write("- name: My Playbook\n")
+    f.write("  hosts: all\n")
+    f.write("  become: true\n")
+    f.write("  tasks:\n")
+    f.write("    - name: Start servers\n")
+    f.write("      script: /users/loukis/Thesis/BFT-Distributed-G-Set-V2/server/main.go\n")
+    f.close()
+
+    os.system("ansible-playbook -i ./hosts start_servers.yml")
+    # os.remove("hosts")
