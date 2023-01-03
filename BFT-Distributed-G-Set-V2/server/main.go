14f459c403b7e842f9fc515a03f43a031a3e0154
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-01-03 08:56:55 -0600

    test

--- BFT-Distributed-G-Set-V2/server/main.go
+++ BFT-Distributed-G-Set-V2/server/main.go
@@ -1,13 +1,21 @@
 package main
 
 import (
-	"backend/config"
-	"backend/scenarios"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
 
-	servers := config.SetServers("LOCAL")
-	scenarios.Start(servers, "NORMAL")
+	// servers := config.SetServers("REMOTE")
+	// scenarios.Start(servers, "NORMAL")
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	fmt.Printf(strings.Split(hostname, ".")[0] + "\n")
+	fmt.Println(os.Args[1])
 
 }
