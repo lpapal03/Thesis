006773bbdfaa0f98a4da3caf7b34589dca65f1c9
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-01-03 10:01:21 -0600

    pogress on server

--- BFT-Distributed-G-Set-V2/server/main.go
+++ BFT-Distributed-G-Set-V2/server/main.go
@@ -1,21 +1,32 @@
 package main
 
 import (
-	"fmt"
+	"backend/config"
+	"backend/server"
 	"os"
+	"strconv"
 	"strings"
 )
 
 func main() {
 
-	// servers := config.SetServers("REMOTE")
-	// scenarios.Start(servers, "NORMAL")
+	// the only thing i know is what i have to do
+	// and the servers in the network
 	hostname, err := os.Hostname()
 	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+		panic(err)
 	}
-	fmt.Printf(strings.Split(hostname, ".")[0] + "\n")
-	fmt.Println(os.Args[1])
+	hostname = strings.Split(hostname, ".")[0]
+	N, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		panic(err)
+	}
+
+	all_servers := []string{}
+	for i := 0; i < N; i++ {
+		p := "node" + strconv.Itoa(i) + ":"
+		all_servers = append(all_servers, p+config.DEFAULT_PORT)
+	}
+	server.Create(hostname, all_servers)
 
 }
--- BFT-Distributed-G-Set-V2/server/server/server.go
+++ BFT-Distributed-G-Set-V2/server/server/server.go
@@ -12,32 +12,32 @@ type Server struct {
 	Zctx           *zmq.Context
 	Peers          []*zmq.Socket
 	Receive_socket zmq.Socket
-	Host           string
-	Port           string
+	Hostname       string
 	Id             string
 	Gset           map[string]string
 	BRB            map[string]bool
 }
 
-func Create(node config.Node, peers []config.Node) Server {
-	id := node.Host + node.Port
+func Create(hostname string, peers []string) Server {
+	id := hostname
 	zctx, _ := zmq.NewContext()
 	server_sockets := make([]*zmq.Socket, 0)
 	my_gset := gset.Create()
 	brb := make(map[string]bool)
 	receive_socket, _ := zctx.NewSocket(zmq.ROUTER)
-	receive_socket.Bind("tcp://*:" + node.Port)
-	tools.Log(id, "Bound tcp://*:"+node.Port)
+	receive_socket.Bind("tcp://*:" + config.DEFAULT_PORT)
+	tools.Log(id, "Bound tcp://*:"+config.DEFAULT_PORT)
 
 	// Connect my dealer sockets to all other servers' router
 	for i := 0; i < len(peers); i++ {
 		// Connect if not me
-		if peers[i] == node {
+		if peers[i] == hostname {
 			continue
 		}
 		s, _ := zctx.NewSocket(zmq.DEALER)
 		s.SetIdentity(id)
-		s.Connect("tcp://" + peers[i].Host + peers[i].Port)
+		s.Connect("tcp://" + peers[i])
+		tools.Log(hostname, "Connected to "+peers[i])
 		// append socket to socket list
 		server_sockets = append(server_sockets, s)
 	}
@@ -46,9 +46,8 @@ func Create(node config.Node, peers []config.Node) Server {
 		Zctx:           zctx,
 		Peers:          server_sockets,
 		Receive_socket: *receive_socket,
-		Host:           node.Host,
-		Port:           node.Port,
 		Id:             id,
+		Hostname:       hostname,
 		Gset:           my_gset,
 		BRB:            brb,
 	}
