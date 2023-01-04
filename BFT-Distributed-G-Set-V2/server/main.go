c821e6f7aec36a1599a7185ae3c48f1db16178e2
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-01-04 06:56:29 -0600

    serverside changes

--- BFT-Distributed-G-Set-V2/server/main.go
+++ BFT-Distributed-G-Set-V2/server/main.go
@@ -1,34 +1,34 @@
 package main
 
 import (
-	"fmt"
 	"os"
 	"strings"
+	"backend/server"
 )
 
 func main() {
 
 	// the only thing i know is what i have to do
 	// and the servers in the network
-	// hostname, err := os.Hostname()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// hostname = strings.Split(hostname, ".")[0]
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	hostname = strings.Split(hostname, ".")[0]
 
 	data, err := os.ReadFile("/users/loukis/Thesis/BFT-Distributed-G-Set-V2/server/hosts")
 	if err != nil {
 		panic(err)
 	}
-	hosts := strings.Split(string(data), "\n")
-	fmt.Println(hosts)
+	hosts := strings.Split(strings.ReplaceAll(string(data) , "\n\n", "\n"), "\n")
+	hosts = hosts[:len(hosts)-1]
+	for i:=0; i<len(hosts); i++{
+		if hosts[i] == "[servers]"{
+			hosts = hosts[i+1:]
+			break
+		}
+	} 
 
-	// all_servers := []string{}
-	// for i := 0; i < N; i++ {
-	// 	p := "node" + strconv.Itoa(i) + ":"
-	// 	all_servers = append(all_servers, p+config.DEFAULT_PORT)
-	// }
-	// server.Create(hostname, all_servers)
-	// fmt.Println("Hello from server", hostname)
+	server.Create(hostname, hosts)
 
 }
