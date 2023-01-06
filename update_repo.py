208cdfc19060257b13d305a09c6ad9faadb384b1
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-01-06 03:53:08 -0500

    added update repo file

---
+++ update_repo.py
@@ -0,0 +1,21 @@
+import os
+
+def IsHostActive(hostname):
+    response = os.system("ping -c 1 " + hostname)
+    if response == 0:
+        return True
+    else:
+        return False
+
+if __name__ == '__main__':
+    f = open("hosts", "w")
+    for i in range(9999):
+        active = IsHostActive("node" + str(i))
+        if active:
+            f.write("node" + str(i) + "\n")
+        if not active:
+            break
+    f.close()
+    os.system("ansible-playbook -i ./hosts update_repo.yml -v")
+    os.remove("hosts")
+    
