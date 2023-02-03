8889086b54bbb657e78d2ae3e682405a757bbcde
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2023-02-03 10:13:24 +0200

    nothing

--- 2-Atomic-Adds-Local/bdso-2/config/setup.go
+++ 2-Atomic-Adds-Local/bdso-2/config/setup.go
@@ -1,5 +1,13 @@
 package config
 
+import (
+	"bufio"
+	"os"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
 type Node struct {
 	Host string
 	Port string
@@ -14,8 +22,56 @@ var (
 	SERVERS          []Node
 )
 
+func parseHostsFile(fileName string, bdso string) ([]Node, error) {
+	var nodes []Node
+	var tagFound bool
+	var min_port int
+	var max_port int
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.Contains(line, "["+bdso+"]") {
+			tagFound = true
+			continue
+		}
+		if tagFound {
+			port_range := strings.Split(line, "-")
+			min_port, err = strconv.Atoi(port_range[0])
+			if err != nil {
+				panic(err)
+			}
+			max_port, err = strconv.Atoi(port_range[1])
+			if err != nil {
+				panic(err)
+			}
+			break
+		}
+	}
+	for p := min_port; p <= max_port; p++ {
+		nodes = append(nodes, Node{Host: "localhost:", Port: strconv.Itoa(p)})
+	}
+	return nodes, nil
+}
+
 func SetServerNodes() []Node {
-	SERVERS = LOCAL_SERVERS
+
+	working_dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	parent_dir := filepath.Dir(working_dir)
+	bdso_name := filepath.Base(working_dir)
+
+	SERVERS, err = parseHostsFile(parent_dir+"/hosts", bdso_name)
+	if err != nil {
+		panic(err)
+	}
 
 	N = len(SERVERS)
 
