ed3b626f2f67f2a40f70712a7f69693292911284
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2023-03-02 20:02:56 +0200

    addes scripts in local

--- 2-Atomic-Adds-Local/client/main.go
+++ 2-Atomic-Adds-Local/client/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"frontend/client"
-	"frontend/config"
-	"frontend/messaging"
+	"fmt"
+	"frontend/modules"
 	"frontend/tools"
+	"os"
+	"strconv"
 
 	zmq "github.com/pebbe/zmq4"
 )
@@ -14,33 +15,33 @@ func main() {
 
 	zctx, _ := zmq.NewContext()
 
-	config.Initialize("sbdso")
-	servers := config.SERVERS
+	// config.Initialize("sbdso")
+	// servers := config.SERVERS
 
-	c1 := client.CreateClient("loukas", servers, zctx)
-	c2 := client.CreateClient("marios", servers, zctx)
+	// c1 := client.CreateClient("loukas", servers, zctx)
+	// c2 := client.CreateClient("marios", servers, zctx)
 
-	go messaging.AddAtomic(c1, "marios;bdso-1;1;2")
-	go messaging.AddAtomic(c2, "loukas;bdso-2;2;1")
+	// go messaging.AddAtomic(c1, "marios;bdso-1;1;2")
+	// go messaging.AddAtomic(c2, "loukas;bdso-2;2;1")
 
-	select {}
+	// select {}
 
-	// if len(os.Args) == 1 {
-	// 	fmt.Println("Not enough arguments")
-	// 	return
-	// }
-	// if len(os.Args) == 2 {
-	// 	start.StartInteractive(zctx, os.Args[1])
-	// 	return
-	// }
-	// if len(os.Args) == 5 {
-	// 	if os.Args[2] == "a" || os.Args[2] == "automated" {
-	// 		client_count, _ := strconv.Atoi(os.Args[3])
-	// 		request_count, _ := strconv.Atoi(os.Args[4])
-	// 		start.StartAutomated(zctx, client_count, request_count, os.Args[1])
-	// 	}
-	// 	return
-	// }
-	// fmt.Println("Invalid arguments")
-	// return
+	if len(os.Args) == 1 {
+		fmt.Println("Not enough arguments")
+		return
+	}
+	if len(os.Args) == 2 {
+		modules.StartInteractive(zctx, os.Args[1])
+		return
+	}
+	if len(os.Args) == 5 {
+		if os.Args[2] == "a" || os.Args[2] == "automated" {
+			client_count, _ := strconv.Atoi(os.Args[3])
+			request_count, _ := strconv.Atoi(os.Args[4])
+			modules.StartAutomated(zctx, client_count, request_count, os.Args[1])
+		}
+		return
+	}
+	fmt.Println("Invalid arguments")
+	return
 }
