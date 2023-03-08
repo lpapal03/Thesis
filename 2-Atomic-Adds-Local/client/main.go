90cf62a6eefb0e5fd13e1f2ba0e32bb5c99d25b7
loukaspapalazarou <loukas.papalazarou.cy@gmail.com>
2023-03-08 10:53:14 +0200

    improved client

--- 2-Atomic-Adds-Local/client/main.go
+++ 2-Atomic-Adds-Local/client/main.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"fmt"
+	"flag"
 	"frontend/modules"
 	"frontend/tools"
-	"os"
-	"strconv"
 
 	zmq "github.com/pebbe/zmq4"
 )
@@ -15,33 +13,21 @@ func main() {
 
 	zctx, _ := zmq.NewContext()
 
-	// config.Initialize("sbdso")
-	// servers := config.SERVERS
+	var bdso string
+	var auto bool
+	var clients int
+	var reqs int
 
-	// c1 := client.CreateClient("loukas", servers, zctx)
-	// c2 := client.CreateClient("marios", servers, zctx)
+	flag.StringVar(&bdso, "net", "", "Bdso network")
+	flag.BoolVar(&auto, "auto", false, "Automated")
+	flag.IntVar(&clients, "clients", 1, "Amount of automated clients")
+	flag.IntVar(&reqs, "reqs", 5, "Amount of requests")
 
-	// go messaging.AddAtomic(c1, "marios;bdso-1;1;2")
-	// go messaging.AddAtomic(c2, "loukas;bdso-2;2;1")
+	flag.Parse()
 
-	// select {}
-
-	if len(os.Args) == 1 {
-		fmt.Println("Not enough arguments")
+	if auto {
+		modules.StartAutomated(zctx, clients, reqs, bdso)
 		return
 	}
-	if len(os.Args) == 2 {
-		modules.StartInteractive(zctx, os.Args[1])
-		return
-	}
-	if len(os.Args) == 5 {
-		if os.Args[2] == "a" || os.Args[2] == "automated" {
-			client_count, _ := strconv.Atoi(os.Args[3])
-			request_count, _ := strconv.Atoi(os.Args[4])
-			modules.StartAutomated(zctx, client_count, request_count, os.Args[1])
-		}
-		return
-	}
-	fmt.Println("Invalid arguments")
-	return
+	modules.StartInteractive(zctx, bdso)
 }
--- 2-Atomic-Adds/client/main.go
+++ 2-Atomic-Adds/client/main.go
@@ -3,37 +3,31 @@ package main
 import (
 	"2-Atomic-Adds/modules"
 	"2-Atomic-Adds/tools"
-	"fmt"
-	"os"
-	"strconv"
+	"flag"
 
 	zmq "github.com/pebbe/zmq4"
 )
 
 func main() {
 	tools.ResetLogFile()
+
 	zctx, _ := zmq.NewContext()
 
-	if len(os.Args) == 1 {
-		fmt.Println("Not enough arguments")
+	var bdso string
+	var auto bool
+	var clients int
+	var reqs int
+
+	flag.StringVar(&bdso, "net", "", "Bdso network")
+	flag.BoolVar(&auto, "auto", false, "Automated")
+	flag.IntVar(&clients, "clients", 1, "Amount of automated clients")
+	flag.IntVar(&reqs, "reqs", 5, "Amount of requests")
+
+	flag.Parse()
+
+	if auto {
+		modules.StartAutomated(zctx, clients, reqs, bdso)
 		return
 	}
-	if len(os.Args) == 2 {
-		modules.StartInteractive(zctx, os.Args[1])
-	}
-	if len(os.Args) == 5 {
-		if os.Args[2] == "a" || os.Args[2] == "automated" {
-			client_count, err := strconv.Atoi(os.Args[3])
-			if err!=nil{
-				panic(err)
-			}
-			request_count, err := strconv.Atoi(os.Args[4])
-						if err!=nil{
-				panic(err)
-			}
-			modules.StartAutomated(zctx, client_count, request_count, os.Args[1])
-		}
-	}
-	fmt.Println("Invalid arguments")
-	return
+	modules.StartInteractive(zctx, bdso)
 }
