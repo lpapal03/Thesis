2e4de5c8632ba7be1bd7f60e8238cc7c8fa535ce
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-03-27 08:04:57 -0500

    testing

--- BFT-Distributed-G-Set-Remote/server/tools/evaluation.go
+++ BFT-Distributed-G-Set-Remote/server/tools/evaluation.go
@@ -1,6 +1,10 @@
 package tools
 
-import "sync"
+import (
+	"fmt"
+	"os"
+	"sync"
+)
 
 var (
 	BRB_MESSAGES    = 0
@@ -9,14 +13,31 @@ var (
 
 var counter_mutex sync.Mutex
 
+func saveState() error {
+	file, err := os.OpenFile("experiment_results.txt", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = fmt.Fprintf(file, "BRB_MESSAGES=%d\nNORMAL_MESSAGES=%d\nTOTAL=%d\n", BRB_MESSAGES, NORMAL_MESSAGES, BRB_MESSAGES+NORMAL_MESSAGES)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func IncrementBRBCount() {
 	counter_mutex.Lock()
 	BRB_MESSAGES++
+	saveState()
 	counter_mutex.Unlock()
 }
 
 func IncrementNormalCount() {
 	counter_mutex.Lock()
 	NORMAL_MESSAGES++
+	saveState()
 	counter_mutex.Unlock()
 }
