838ef96f95404062576a18adea822e46acc10f87
Loukas Papalazarou <loukas.papalazarou.cy@gmail.com>
2023-04-10 10:58:56 -0500

    bug fux

--- BFT-Distributed-G-Set-Remote/run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-nohup ./run_all.sh > logfile 2>&1 &
--- BFT-Distributed-G-Set-Remote/run_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm -rf results
-mkdir results
-
-./run_scenario.sh normal
-./run_scenario.sh mute
-./run_scenario.sh malicious
-./run_scenario.sh half_and_half
